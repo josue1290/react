b80853bb9bda7f17eecda70278c8e9170fdb416c
josue1290 <ti1601001@academica.utbb.edu.mx>
2022-10-20 19:27:58 -0500

    segundo

--- directorio/src/App.js
+++ directorio/src/App.js
@@ -4,17 +4,9 @@ import React from 'react';
 
 function App() {
 
-  let midato=12;
-  let midato2=13;
-
   return (
-  // React.Fragment sirve para envolver varias etiquetas
-  // se puede trabajar como un arreglo al poner [] en lugar de los corchetes
-    <React.Fragment>
-      <h1>hola mundo</h1>
-      <h1>{midato + midato2}</h1>
-    </React.Fragment>
-  );
+      
+    );
 }
 
 export default App;
